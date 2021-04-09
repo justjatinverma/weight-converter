dad057864ce168bc0d0dd7c7071957820926978e
Jatin Verma <82190096+Leenoblade@users.noreply.github.com>
2021-04-09 10:09:20 +0530

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ document.getElementById('lbsinput').addEventListener('input',function(e)
 {
     document.getElementById('output').style.visibility = 'visible';
     let lbs = e.target.value;
-    document.getElementById('gramsoutput').innerHTML = lbs/0.022046;
+    document.getElementById('gramsoutput').innerHTML = lbs*453.592;
     document.getElementById('kgoutput').innerHTML = lbs/2.2046;
     document.getElementById('ozeoutput').innerHTML = lbs*16;
 });
